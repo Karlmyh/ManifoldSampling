eda762226b150316957570168c1d3e23bec5bdcc
Ma Yuheng <1075288776@qq.com>
2022-07-02 11:34:11 +0800

    update

--- Thesis/compile.sh
+++
@@ -1,4 +0,0 @@
-xelatex -synctex=1 -interaction=nonstopmode -file-line-error -pdf sample
-bibtex sample
-xelatex -synctex=1 -interaction=nonstopmode -file-line-error -pdf sample
-xelatex -synctex=1 -interaction=nonstopmode -file-line-error -pdf sample
